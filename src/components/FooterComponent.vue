a95019906e2e5de3d97444ec458cb572bebd088d
Henrique0330 <henriquebelboni@gmail.com>
2024-08-28 11:24:10 -0300

    feat-3: style

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,41 @@
+<template>
+      <footer>
+    <h1 class="Tfooter">BIBLIOTECA</h1>
+    <div class="footerP">
+      <h2>BIBLI OTECA® É UMA MARCA REGISTRADA, CRIADA EM 2024.
+        CNPJ: XX.XXX.XXX/XXXX-XX</h2>
+      <h3>Rua Davi da rosa, 584, Aventureiro, Joinville-SC - 89226-435</h3>
+      <hr>
+      <h2>Os termos e condições apresentados em nosso site são exclusivos para empréstimos online e podem variar em
+        relação
+        às condições das bibliotecas físicas. As informações divulgadas neste site ou em nossos e-mails promocionais
+        estão
+        sujeitas a alterações sem aviso prévio. A Biblioteca Online não se responsabiliza por erros nas descrições dos
+        materiais. As imagens exibidas nesta página são meramente ilustrativas e podem variar de acordo com a editora ou
+        a
+        edição disponível. Empréstimos válidos até o término da disponibilidade dos títulos. Acesso aos materiais está
+        sujeito à análise e confirmação dos dados do usuário.</h2>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+
+footer {
+  width: 100%;
+  height: 440px;
+  background-color: #8FD085;
+  display: flex;
+}
+
+.footerP {
+  margin-top: 20px;
+  padding: 10px 400px 30px 400px;
+  text-align: center;
+  color: #FFFFFF;
+}
+
+.Tfooter {
+  color: #FFFFFF;
+}
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,73 @@
+<template>
+    <header>
+        <div>
+            <h1>Biblioteca</h1>
+        </div>
+        <div class="barra">
+            <img src="../assets/logo.svg" alt="Ícone de pesquisa" class="icon">
+            <input type="pesquisa" name="pesquisa" placeholder="Buscar">
+        </div>
+    </header>
+</template>
+
+<style scoped>
+
+header {
+    display: flex;
+    justify-content: space-evenly;
+    width: 100%;
+    height: 120px;
+    margin: 0 0 3rem 0;
+    background-color: #8FD085;
+}
+header div h1{
+    margin-top: 3rem;
+}
+
+.barra {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+}
+
+.barra img.icon {
+    width: 20px;
+    height: auto;
+    position: absolute;
+    left: 10px;
+
+}
+
+.barra input {
+    padding-left: 40px;
+    width: 100%;
+    max-width: 300px;
+    height: 36px;
+    border-radius: 10px;
+    border: 1px solid #FFFFFF;
+    background-color: #8FD085;
+    color: white;
+    font-size: 16px;
+}
+
+.barra input::placeholder {
+    color: white;
+    font-size: 15px;
+}
+
+
+@media (max-width: 768px) {
+    .barra input {
+        max-width: 250px;
+        padding-left: 35px;
+    }
+}
+
+@media (max-width: 480px) {
+    .barra input {
+        max-width: 200px;
+        padding-left: 30px;
+    }
+}
+</style>
